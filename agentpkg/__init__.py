f2fc15eac206d617493159eb72d3d3ccf28e4044
job van creij <jobvancrey@hotmail.com>
2025-05-29 12:41:38 +0200

    update

--- agentpkg/__init__.py
+++ agentpkg/__init__.py
@@ -7,17 +7,17 @@ try:
 except metadata.PackageNotFoundError:  # pragma: no cover - package not installed
     __version__ = "0.0.0"
 
-from .graph_helpers.call_subgraph import (
+from agentpkg.graph_helpers.call_subgraph import (
     call_azure_function,
     call_subgraph,
     FUNCTION_KEY,
     FunctionKeySpec,
 )
-from .graph_helpers.graph_builder_helpers import parse_json
-from .graph_helpers.wrappers import validate_body, skip_if_locked
-from .graph_endpoints.graph_executor_factory import EndpointGenerator
-from .graph_endpoints.graph_executor_service import GraphExecutorService
-from .graph_endpoints.registry import APIRegistry, Endpoint, registry
+from agentpkg.graph_helpers.graph_builder_helpers import parse_json
+from agentpkg.graph_helpers.wrappers import validate_body, skip_if_locked
+from agentpkg.graph_endpoints.graph_executor_factory import EndpointGenerator
+from agentpkg.graph_endpoints.graph_executor_service import GraphExecutorService
+from agentpkg.graph_endpoints.registry import APIRegistry, Endpoint, registry
 from .logger import get_logger
 from .settings import settings
 
